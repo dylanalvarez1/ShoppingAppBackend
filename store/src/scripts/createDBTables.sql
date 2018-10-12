509d8447d3f2e1f1e6301413bcb742bacfb8a572
dylanalvarez1 <dylanalvarez1@ufl.edu>
2018-10-12 13:58:06 -0400

    update sql in scripts folder to include more tables
    There is now a orders and cart table to support those functionalities in the future

--- store/src/scripts/createDBTables.sql
+++ store/src/scripts/createDBTables.sql
@@ -1,6 +1,5 @@
-USE db_store;
 SHOW TABLES;
-DROP TABLE IF EXISTS products, customers, orders;
+DROP TABLE IF EXISTS products, customers, orders, carts;
 SHOW TABLES;
 CREATE TABLE customers(id SERIAL, fname VARCHAR(255), lname VARCHAR(255), 
 		username VARCHAR(255),email VARCHAR(255), UNIQUE(id, username));
@@ -8,6 +7,10 @@ CREATE TABLE customers(id SERIAL, fname VARCHAR(255), lname VARCHAR(255),
 CREATE TABLE products(itemId SERIAL, name VARCHAR(255), msrp DECIMAL(6,2), salePrice DECIMAL(8,2), 
 	upc INT, shortDescription VARCHAR(255), brandName VARCHAR(255), 
 	size VARCHAR(255), color VARCHAR(255), gender VARCHAR(255), UNIQUE(itemId));
+
+CREATE TABLE orders(orderId SERIAL, itemId VARCHAR(255), cartId DECIMAL(6,2), UNIQUE(orderId));
+
+CREATE TABLE carts(cartId SERIAL, userID INT, status BOOLEAN);
 
 SELECT "You will need to decide how to persist cart and order information. Will you amend an existing table or create new one(s)?" AS "";
 SHOW TABLES;
